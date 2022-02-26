f4103af5d577b81848cc79966da4d68fc1d4f58f
Haidarali Masu <haidaralimasu123@gmail.com>
2022-02-26 16:35:08 +0530

    Update Minter.tsx

--- src/components/Minter.tsx
+++ src/components/Minter.tsx
@@ -98,7 +98,7 @@ const Minter = () => {
         {/* <img src={banner} className="banner" alt="banner" /> */}
 
         {/* <h1 className="minter-h1">Mint Your Meta Estate NFT</h1> */}
-        <h1 className="minter-h1">Public Sale Starts On March 1</h1>
+        <h1 style={{color:"white"}} className="minter-h1">Presale 28th of February | Public Sale 1st of March</h1>
 
         {totalSupply < maxSupply ? (
           <div className="minting-section">
