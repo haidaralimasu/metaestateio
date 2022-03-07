8b4ec7e5ec8d2f40d48e874d4d358c66cff3ad4d
Haidarali Masu <haidaralimasu123@gmail.com>
2022-03-07 16:26:05 +0530

    Update Minter.tsx

--- src/components/Minter.tsx
+++ src/components/Minter.tsx
@@ -98,7 +98,7 @@ const Minter = () => {
         {/* <img src={banner} className="banner" alt="banner" /> */}
 
         {/* <h1 className="minter-h1">Mint Your Meta Estate NFT</h1> */}
-        <h1 style={{color:"white"}} className="minter-h1">MetaEstate Mint Is Live Now</h1>
+        <h1 style={{color:"white"}} className="minter-h1">Mint Is Live On Friday, 11th Of March 3AM PST</h1>
 
         {totalSupply < maxSupply ? (
           <div className="minting-section">
@@ -209,7 +209,7 @@ const Minter = () => {
             <div className="minter-status-card">
               <h6>Status</h6>
 
-              <h2>Live</h2>
+              <h2>Live On 11th March</h2>
             </div>
             <div className="vl"></div>
             <div className="minter-status-card">
